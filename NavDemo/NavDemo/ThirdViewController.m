0e9a45179e9bfc0663f6b6402fa3e96bfc3a4c0d
abemassry <abe@massindustries.org>
2014-03-15 13:23:46 -0400

    commenting

--- NavDemo/NavDemo/ThirdViewController.m
+++ NavDemo/NavDemo/ThirdViewController.m
@@ -14,6 +14,8 @@
 
 @implementation ThirdViewController
 
+
+//nib from the interface builder
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
